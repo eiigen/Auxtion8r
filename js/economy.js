bf1af693fc4498c56b53d03b5c1429af1342d83a
eiigen <yugenshi5@gmail.com>
2026-07-18 22:24:04 +0800

    Rewrite economy.js

--- js/economy.js
+++ js/economy.js
@@ -1,4 +1,4 @@
-function calcIncome(s){return s.inventory.reduce((a,id)=>(getItem(id)?.income||0)+a,0)}
+function calcIncome(s){return s.inventory.reduce(function(a,id){return (getItem(id)?getItem(id).income:0)+a},0)}
 function calcPassive(s){return Math.floor(calcIncome(s)*((Date.now()-s.lastPassive)/60000))}
-function addPassive(s){const e=calcPassive(s);s.coins+=e;s.earned+=e;s.lastPassive=Date.now();return{state:s,earned:e}}
+function addPassive(s){var e=calcPassive(s);s.coins+=e;s.earned+=e;s.lastPassive=Date.now();return{state:s,earned:e}}
 function canAfford(s,a){return s.coins>=a}
